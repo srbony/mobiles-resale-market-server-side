22f0508327701b8a3b779f65c3d4d4a2340b1821
srbony <srabonycse@gmail.com>
2022-11-24 19:10:49 +0600

    added port

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
+const port = process.env.PORT || 5000;
 
 
 //midleware
@@ -11,5 +12,8 @@ app.use(express.json());
 
 
 app.get('/', async (req, res) => {
-    res.send('products portal is running');
-})
+    res.send('Resale  products portal is running');
+});
+
+app.listen(port, () => console.log(`Resale products portal running on ${port}`))
+
